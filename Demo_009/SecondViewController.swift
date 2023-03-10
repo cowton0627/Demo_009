138319309ac8c7233faf22f544434ad1d9d7ac22
Charles Cheng <cowton0517@gmail.com>
2023-03-10 14:52:01 +0800

    another modify

--- Demo_009/SecondViewController.swift
+++ Demo_009/SecondViewController.swift
@@ -9,33 +9,31 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
-    @IBOutlet weak var myImageView: UIImageView!
     @IBOutlet weak var myMiddleImageView: UIImageView!
+    @IBOutlet weak var myBottomImageView: UIImageView!
     
-    //回上一頁的動作
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // 取用 Asset 的 Gif 圖，將之顯示於 myBottomImageView
+        guard let data = NSDataAsset(name: "q8bTGuy-2")?.data else { return }
+        let cfData = data as CFData
+        CGAnimateImageDataWithBlock(cfData, nil) { (_, cgImage, _) in
+            self.myBottomImageView.image = UIImage(cgImage: cgImage)
+        }
+    }
+    
+    // 回上一頁的動作
     @IBAction func dismissTapped(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
     
-    
-    
 //    @IBAction func goBacktoMain(segue: UIStoryboardSegue) {
-//        if let controller = storyboard?.instantiateViewController(withIdentifier: "Main"){
+//        if let controller = storyboard?.instantiateViewController(withIdentifier: "Main") {
 //            present(controller, animated: true, completion: nil)
 //            print("going back")
 //        }
-//       }
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        guard let data = NSDataAsset(name: "q8bTGuy-2")?.data else { return }
-        let cfData = data as CFData
-        CGAnimateImageDataWithBlock(cfData, nil) { (_, cgImage, _) in
-                 self.myImageView.image = UIImage(cgImage: cgImage)
-        }
-    }
+//    }
     
     
 }
